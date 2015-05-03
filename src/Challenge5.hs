43bc613a6d635747d475a873f9b0b79104174b13
Michael Kane <m.k.kane@gmail.com>
2015-05-03 19:42:13 +0100

    challenge5 implemented

--- src/Challenge5.hs
+++ src/Challenge5.hs
@@ -4,13 +4,38 @@ import Data.ByteString.Lazy.Internal (ByteString)
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BC
 import qualified Data.Bits as Bits
--- import qualified Helpers
+import Options.Applicative
 
 encrypt :: ByteString -> ByteString -> ByteString
 encrypt key plaintext = B.pack $ B.zipWith (Bits.xor) repeatedKey plaintext
-  where repeatedKey = BC.concat $ repeat key
+  where repeatedKey = B.concat $ repeat key
 
--- Read key from command line and plaintext from stdin
+encryptStdinWithKey :: String -> IO ()
+encryptStdinWithKey key = B.interact $ encrypt (BC.pack key)
+
+
+keyOpt :: Parser String
+keyOpt = strOption
+      ( long "key"
+        <> short 'k'
+        <> metavar "KEY"
+        <> help "Encryption key" )
+
+description :: String
+description = "Encode stdin using KEY via repeating-key XOR.  (Note that\
+               \ output is in bytes, so to convert to hex try piping to\
+               \`xxd -p`)"
+
 main :: IO ()
-main = do
-   BC.interact $ encrypt (BC.pack "key")
+main = execParser opts >>= encryptStdinWithKey
+  where
+    opts = info (helper <*> keyOpt)
+           ( fullDesc
+             <> progDesc description
+             <> header "repeating-key XOR encryption" )
+
+
+-- -- Read key from command line and plaintext from stdin
+-- main :: IO ()
+-- main = do
+--    B.interact $ encrypt (B.pack [2,34,4,23])
