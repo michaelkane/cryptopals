7bd75560b2cd1f35ff493a20d67f2282b10ffc7b
Michael Kane <m.k.kane@gmail.com>
2015-06-13 17:46:33 +0100

    challenge8 done (I think)

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,8 @@ module Main where
 -- import qualified Challenge4
 -- import qualified Challenge5
 -- import qualified Challenge6
-import qualified Challenge7
+-- import qualified Challenge7
+import qualified Challenge8
 
 main :: IO()
-main = Challenge7.main
+main = Challenge8.main
